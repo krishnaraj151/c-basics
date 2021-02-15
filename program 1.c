235adf7fd78ea1b1e04fee2c4b5e1d7b1c2bc646
krishnaraj151 <77149970+krishnaraj151@users.noreply.github.com>
2021-02-15 08:26:58 -0800

    Add files via upload

---
+++ program 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("enter the number ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            printf("%d",i+1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
---
+++ program 4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("enter the number ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d",i+1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
